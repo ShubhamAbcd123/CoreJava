ed8448806f35250b9770fc9dcc25851d4bdea2d2
Shubham Vishwakarma <36370464+ShubhamAbcd123@users.noreply.github.com>
2019-02-17 23:17:21 +0530

    Example1 is created

---
+++ Example1.java
@@ -0,0 +1,35 @@
+package demoinheritance;
+public class Super {
+    public static void main(String [] args)
+    {
+        Employe e= new Employe();
+        e.dis();
+    }
+}
+class Person
+{
+    String Fname;
+    String Lname;
+    Person()
+    {
+        this.Fname="Shubham";
+        this.Lname="Karma";
+    }
+    public void dis()
+    {
+        System.out.println(Fname + " " + Lname);
+    }
+}
+class Employe extends Person
+{
+    String org_name;
+    Employe()
+    {
+        this.org_name="NITT";
+    }
+    @Override
+    public void dis()
+    {
+        System.out.println(Fname + " " + Lname + " " + org_name);
+    }
+}
